6debd705f72d55cefd129bb548027b72b523f6e5
sudha-mv <138387013+sudha-mv@users.noreply.github.com>
2024-02-18 17:36:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+    var i = 0;
+    var txt = 'ORDER LATE NIGHT MEALS'; /* The text */
+    var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+    var video = document.getElementById("myVideo");
